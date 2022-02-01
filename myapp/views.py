65e4cffa00570f9a4145c015ae002741f261be44
Naman82 <b520031@iiit-bh.ac.in>
2022-02-02 04:05:13 +0530

    login and register connected

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html')
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
