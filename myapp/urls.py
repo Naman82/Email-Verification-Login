6b4ad40599cd383ab7691ad4ec1dc17ba3f11a70
Naman82 <b520031@iiit-bh.ac.in>
2022-02-02 11:09:21 +0530

    register successful

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.login,name='login'),
     path('register/',views.register,name='register'),
+    path('success/',views.success,name='success')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
     return render(request,'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Already Used!')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+        else:
+            messages.info(request,'Password did not match, Try Again!')
+            return redirect('register')
+       
     return render(request,'register.html')
+
+
+def success(request):
+    return render(request,'success.html')
